8fe6d66c5319511448205fd31876461dd4573c70
ErwinJ16 <158303409+ErwinJ16@users.noreply.github.com>
2024-02-13 09:21:25 -0600

    Add files via upload

---
+++ asistenciaalumnos.py
@@ -0,0 +1,9 @@
+for _ in range(10):
+    carne=input("ingrese número de carné")
+    if len(carne)!=8:
+        print("Carné inválido")
+    elif len(carne)==8:
+        print("Carné válido")
+for _ in range(10):
+    nombreyapellido=input("Ingrese un nombre y un apellido")
+
